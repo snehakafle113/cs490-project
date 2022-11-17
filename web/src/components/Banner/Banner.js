7599fdeeb8f5f2fbc39f1d24d457f1581063c5f9
tg283 <tg283@njit.edu>
2022-11-17 00:03:10 +0000

    Create banner component

---
+++ web/src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+const Banner = () => {
+  return (
+    <div>
+      <h2>{'Banner'}</h2>
+      <p>{'Find me in ./web/src/components/Banner/Banner.js'}</p>
+    </div>
+  )
+}
+
+export default Banner
