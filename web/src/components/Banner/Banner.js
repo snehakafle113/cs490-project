11103fced59de806f3015be4d27b4234e4b196fc
Jayhawk416 <jwh24@njit.edu>
2022-11-20 19:17:46 -0500

    Changed Banner to have the desiried logo on it

--- web/src/components/Banner/Banner.js
+++ web/src/components/Banner/Banner.js
@@ -1,12 +1,15 @@
 import {
    Box
 } from '@chakra-ui/react'
-
+import Logo from 'src/assets/logo.png'
+import React from 'react'
 const Banner = () => {
   return (
     <Box bg='#26619C' w='100%' p={4} color='white'>
-      <p>EnchantingTable</p>
-      <Image src={/assets/logo.png}/>
+      <p>Enchanting Table</p>
+      <div>
+        <img src={Logo} alt="image" width="50" height="50"/>
+      </div>
     </Box>
   )
 }
