c81ccf431fa457eb0b063218de4845faee2be84e
Jayhawk416 <jwh24@njit.edu>
2022-11-29 22:34:55 -0500

    Completely rewrote the weekView.js to instead use the FullCalendar.io for building the calendar view. Added storybook to weekView as well as updated yarn.lock with files added

--- web/src/components/weekView/weekView.js
+++ web/src/components/weekView/weekView.js
@@ -1,42 +1,22 @@
-function Day(i){// day needs 2 parameters: day of the week and date
-  var q = [
-      'Sunday',
-      'Monday',
-      'Tuesday',
-      'Wednesday',
-      'Thursday',
-      'Friday',
-      'Saturday',
-    ];
-  var hours = new Array(5);
-  for(var j = 0; j < hours.length; j++){
-      hours[j] = j;
-  }
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
-  return(q[i]);
-
+function weekView(){
+    return (
+        <div>
+            <FullCalendar
+                defaultView="timeGridWeek"
+                header={{
+                    left: "prev,next,today",
+                    center: "title",
+                    right: "timeGridWeek,timeGridDay"
+                }}
+                plugins={[timeGridPlugin,dayGridPlugin]}
+            />
+        </div>
+    );
 }
 
-function Week(){//week needs 1 parameter: day of the week
-  var week = new Array(1);
-  for(var i = 0; i < week.length; i++){
-      week[i] = new Array(7);
-  }
-
-  for(var i = 0; i < 1; i++){
-      for(var j = 0; j < 7; j++){
-          week[i][j] = Day(j);
-      }
-  }
-  for(var i = 0; i < 1; i++){
-      for(var j = 0; j <7; j++){
-          //document.write(week[i][j] + ' ');
-      }
-      //document.write('<br>');
-  }
-  return(week);
-}
-
-console.log(Week())
-
-//export {Week, Day as default};
+export default weekView;
