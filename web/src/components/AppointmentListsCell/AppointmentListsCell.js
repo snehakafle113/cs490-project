4a10d91a63eff3afaeb99f4cf7f024a0588227ed
cheesemid <jcwcopg@gmail.com>
2022-11-30 17:31:47 -0500

    add empty

--- web/src/components/AppointmentListsCell/AppointmentListsCell.js
+++ web/src/components/AppointmentListsCell/AppointmentListsCell.js
@@ -14,7 +14,21 @@ export const QUERY = gql`
 
 export const Loading = () => <div>Loading...</div>
 
-export const Empty = () => <div>Empty</div>
+export const Empty = () => (
+    <div>
+      <Box p="5" borderWidth="1px">
+        <ul>
+          <Text fontSize="xl" fontWeight="semibold" lineHeight="long">
+            Appointments
+          </Text>
+          <li style={{"list-style-type": "none"}}><br></br></li>
+          <Text fontSize="l" lineHeight="long">
+            Empty
+          </Text>
+        </ul>
+      </Box>
+    </div>
+)
 
 export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
