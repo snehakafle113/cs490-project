f82f99cdaf351330d2331439ae64d3bce6bfcb3b
tg283 <tg283@njit.edu>
2022-11-30 20:18:01 +0000

    update banner layout structure via Chakra HStacks

--- web/src/components/Banner/Banner.js
+++ web/src/components/Banner/Banner.js
@@ -1,15 +1,27 @@
 import {
-   Box
+   Box,
+   HStack,
+   Flex,
+   Spacer,
+   Text
 } from '@chakra-ui/react'
 import Logo from 'src/assets/logo.png'
 import React from 'react'
 const Banner = () => {
   return (
     <Box bg='#26619C' w='100%' p={4} color='white'>
-      <p>Enchanting Table</p>
-      <div>
-        <img src={Logo} alt="image" width="50" height="50"/>
-      </div>
+      <Flex>
+        <HStack spacing = '17px'>
+          <Box>
+            <img src={Logo} alt="image" width="50" height="50"/>
+            </Box>
+          <Box>
+            <Text fontsize='4xl' as='b'>EnchantingTable</Text>
+          </Box>
+        </HStack>
+        <Spacer />
+          <img src={Logo} alt="image" width="50" height="50"/>
+      </Flex>
     </Box>
   )
 }
