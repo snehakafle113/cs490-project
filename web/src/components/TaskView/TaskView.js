7623a4cc4f5c8f24e990e369571a9233115bb9e4
TNBerkman <90403029+TNBerkman@users.noreply.github.com>
2022-12-09 08:08:55 -0500

    Created NewTask drawer, mounted NewTask.js to it

--- web/src/components/TaskView/TaskView.js
+++ web/src/components/TaskView/TaskView.js
@@ -1,10 +1,37 @@
+import {
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
+import { Button, useDisclosure } from '@chakra-ui/react'
+
+import NewTask from '../Task/NewTask'
 import TasksCell from '../Task/TasksCell'
 
 const TaskView = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <div>
-      <TasksCell />
-    </div>
+    <view>
+      <div>
+        <TasksCell />
+      </div>
+
+      <Button onClick={onOpen}>New Task</Button>
+      <Drawer isOpen={isOpen} onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerCloseButton />
+          </DrawerHeader>
+          <DrawerBody>
+            <NewTask />
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </view>
   )
 }
 
