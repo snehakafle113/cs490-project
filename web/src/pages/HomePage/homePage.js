6592638eed77f62bf184097d7821011e60042625
TNBerkman <90403029+TNBerkman@users.noreply.github.com>
2022-12-12 11:56:37 -0500

    merge from main but working this time

--- web/src/pages/HomePage/homePage.js
+++ web/src/pages/HomePage/homePage.js
@@ -1,14 +1,20 @@
 // import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Box, Center, Button } from '@chakra-ui/react'
-import CalendarView from '../../components/calendarView'
-import { Text, Input } from "@chakra-ui/react"
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Box,
+  Center,
+} from '@chakra-ui/react'
+import { Text } from '@chakra-ui/react'
 
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 
+import TaskView from 'src/components/TaskView'
+
 import CalendarView from '../../components/calendarView'
-import TaskView from '../../components/TaskView'
 
 const HomePage = () => {
   return (
