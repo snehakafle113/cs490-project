ee23e02cbf80aaee9358e9486198eebffdef2bad
Nico Oberholtzer <Nlo2@njit.edu>
2022-12-11 22:06:41 -0500

    created iteration of login button through firebase in order to test firebase login and authentication

--- web/src/App.js
+++ web/src/App.js
@@ -1,3 +1,6 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import * as firebaseAuth from '@firebase/auth'
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
@@ -13,15 +16,42 @@ import theme from './theme.js'
 
 const extendedTheme = extendTheme(theme)
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+
+  /** Optional config, may be needed, depending on how you use firebase
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  **/
+}
+
+const firebaseApp = ((config) => {
+  const apps = getApps()
+  if (!apps.length) {
+    initializeApp(config)
+  }
+  return getApp()
+})(firebaseConfig)
+
+export const firebaseClient = {
+  firebaseAuth,
+  firebaseApp, // optional
+}
+
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
     <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <ColorModeScript />
-      <ChakraProvider theme={theme}>
-        <RedwoodApolloProvider>
-          <Routes />
-        </RedwoodApolloProvider>
-      </ChakraProvider>
+      <AuthProvider client={firebaseClient} type="firebase">
+        <ColorModeScript />
+        <ChakraProvider theme={theme}>
+          <RedwoodApolloProvider>
+            <Routes />
+          </RedwoodApolloProvider>
+        </ChakraProvider>
+      </AuthProvider>
     </RedwoodProvider>
   </FatalErrorBoundary>
 )
--- web/src/pages/HomePage/homePage.js
+++ web/src/pages/HomePage/homePage.js
@@ -1,38 +1,64 @@
-import { Link, routes } from '@redwoodjs/router'
+// import { Link, routes } from '@redwoodjs/router'
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Box,
+  Center,
+  Button,
+} from '@chakra-ui/react'
+import { Text, Input } from '@chakra-ui/react'
+
+import { useAuth } from '@redwoodjs/auth'
 import { MetaTags } from '@redwoodjs/web'
-import { Tabs, TabList, TabPanels, Tab, TabPanel, Box, Center, Button } from '@chakra-ui/react'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
 //import TaskView from '../../components/Task'
 import CalendarView from '../../components/calendarView'
-import { Text } from "@chakra-ui/react"
-import { Input } from '@chakra-ui/react'
 
 const HomePage = () => {
+  const { loading, isAuthenticated, logIn, logOut, currentUser } = useAuth()
 
   return (
     <>
+      <Button
+        onClick={async () => {
+          if (isAuthenticated) {
+            await logOut()
+          } else {
+            await logIn()
+          }
+        }}
+      >
+        {isAuthenticated ? currentUser.uid : 'Log in'}
+      </Button>
       <MetaTags title="Home" description="Home page" />
       <Tabs>
         <TabList>
-          <Text fontWeight="extrabold" mt='2'>Planner</Text>
+          <Text fontWeight="extrabold" mt="2">
+            Planner
+          </Text>
           <Tab>Calendar</Tab>
           <Tab>Tasks</Tab>
         </TabList>
-      <TabPanels>
-        <TabPanel>
-          <Center>
+        <TabPanels>
+          <TabPanel>
+            <Center>
               <Box w="85%">
-                <CalendarView/>
+                <CalendarView />
               </Box>
-          </Center>
-        </TabPanel>
-        <TabPanel>
-          <Center>
-            <Box w="85%">
-              <></>
-            </Box>
-          </Center>
-        </TabPanel>
-      </TabPanels>
+            </Center>
+          </TabPanel>
+          <TabPanel>
+            <Center>
+              <Box w="85%">
+                <></>
+              </Box>
+            </Center>
+          </TabPanel>
+        </TabPanels>
       </Tabs>
     </>
   )
