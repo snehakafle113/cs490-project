badcc0a36a6b75274f3c0c12e7ce5b4e4e65a781
Jayhawk416 <jwh24@njit.edu>
2022-12-12 02:39:34 -0500

    made more changes in an attempt to have the appointments import into the calendar

--- web/src/pages/HomePage/homePage.js
+++ web/src/pages/HomePage/homePage.js
@@ -2,14 +2,18 @@
 import { MetaTags } from '@redwoodjs/web'
 import { Tabs, TabList, TabPanels, Tab, TabPanel, Box, Center, Button } from '@chakra-ui/react'
 //import TaskView from '../../components/Task'
-import CalendarView from '../../components/calendarView'
+import { CalendarView } from '../../components/calendarView'
 import { Text, Input } from "@chakra-ui/react"
+import AuthorizeCell from 'src/components/AuthorizeCell'
+import AppointmentItemCell from 'src/components/AppointmentItemCell'
 
 const HomePage = () => {
 
   return (
     <>
       <MetaTags title="Home" description="Home page" />
+      <AuthorizeCell user_id={'1111'}></AuthorizeCell>
+      <AppointmentItemCell user_id={'1111'}></AppointmentItemCell>
       <Tabs>
         <TabList>
           <Text fontWeight="extrabold" mt='2'>Planner</Text>
@@ -20,7 +24,7 @@ const HomePage = () => {
         <TabPanel>
           <Center>
               <Box w="85%">
-                <CalendarView/>
+                <CalendarView user_id={'1111'}></CalendarView>
               </Box>
           </Center>
         </TabPanel>
