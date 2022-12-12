e132aec31eaddc9166646e1df88532e196cd378b
TNBerkman <90403029+TNBerkman@users.noreply.github.com>
2022-12-12 12:12:28 -0500

    AppointmentsCell mounted, SimpleGrid formatting

--- web/src/components/TaskView/TaskView.js
+++ web/src/components/TaskView/TaskView.js
@@ -7,7 +7,9 @@ import {
   DrawerCloseButton,
 } from '@chakra-ui/react'
 import { Button, useDisclosure } from '@chakra-ui/react'
+import { SimpleGrid } from '@chakra-ui/react'
 
+import AppointmentsCell from '../Appointment/AppointmentsCell'
 import NewTask from '../Task/NewTask'
 import TasksCell from '../Task/TasksCell'
 
@@ -15,12 +17,13 @@ const TaskView = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <view>
-      <div>
+      <SimpleGrid columns={2} spacingX="40px" spacingY="20px">
         <TasksCell />
-      </div>
+        <AppointmentsCell />
+      </SimpleGrid>
 
       <Button onClick={onOpen}>New Task</Button>
-      <Drawer isOpen={isOpen} onClose={onClose}>
+      <Drawer isOpen={isOpen} onClose={onClose} size={'lg'}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerHeader>
